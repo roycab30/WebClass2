d463020597f0843a754b1e3bebb30f0279fb8581
roycab30 <roy@dgventures.net>
2024-11-01 21:05:20 +1300

    delete node modules

--- dbConfig.js
+++ dbConfig.js
@@ -1,15 +1,19 @@
-
-var mysql = require('mysql')
-var conn  = mysql.createConnection( {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'webclass2db'
-})
-
-conn.connect(function(err) {
-    if (err) throw err
-    console.log('Database connected')
-})
-
-module.exports = conn
+require('dotenv').config();
+const mysql = require('mysql');
+ 
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+ 
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database: ' + err.stack);
+        return;
+    }
+    console.log('Connected to database');
+});
+ 
+module.exports = connection;
